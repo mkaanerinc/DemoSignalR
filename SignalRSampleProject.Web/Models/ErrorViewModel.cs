a7a778ef07b73b6d1adc201788f2ca64d26be27b
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-04-12 13:31:40 +0300

    Created SignalR Sample Project

---
+++ SignalRSampleProject.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalRSampleProject.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
