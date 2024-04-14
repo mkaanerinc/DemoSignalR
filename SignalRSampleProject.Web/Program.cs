efe3dc16b160b15fb474b193c65de17219653fc4
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-04-14 13:40:38 +0300

    Added ClosedXML Library & Background Service

--- SignalRSampleProject.Web/Program.cs
+++ SignalRSampleProject.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using SignalRSampleProject.Web.BackgroundServices;
 using SignalRSampleProject.Web.Models;
 using SignalRSampleProject.Web.Services;
 using System.Threading.Channels;
@@ -9,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory()));
 builder.Services.AddScoped<FileService>();
 builder.Services.AddSingleton(Channel.CreateUnbounded<(string userId, List<Product> products)>());
 //builder.Services.AddSingleton(Channel.CreateUnbounded<Tuple<string,List<Product>>>());
@@ -21,6 +24,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddHostedService<CreateExcelBackgroundService>();
 
 var app = builder.Build();
 
